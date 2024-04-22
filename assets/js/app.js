8e86b9d817f0aa2dc5e06ddd208869dd8f77dda4
Notizou <noah.decroix3@gmail.com>
2024-04-22 16:42:43 +0200

    add starting page and auto generate btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-console.log('tetetete');
+let startBtn = document.querySelector('#startBtn');
+let automaticBtn = document.querySelector('#automatic_btn');
+
+startBtn.addEventListener('click', () => {
+    window.location.href = 'http://localhost:8000/module';
+});
+
+automaticBtn.addEventListener('click', () => {
+    window.location.href = 'http://localhost:8000/module/generate';
+});
+import '../styles/app.css';
