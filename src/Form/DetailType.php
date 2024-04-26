24c3be7a0090966501a9c636b34d59e329bac7d1
Notizou <noah.decroix3@gmail.com>
2024-04-26 14:50:06 +0200

    add security to form

--- src/Form/DetailType.php
+++ src/Form/DetailType.php
@@ -7,16 +7,34 @@ use App\Entity\Module;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class DetailType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('manufacturer')
-            ->add('serialNumber')
+            ->add('manufacturer', TextType::class, [
+                'constraints' => new Length([
+                    'min' => 2,
+                    'minMessage'=> "Le fabricant doit avoir une longueur minimale de 2",
+                    'max' => 50,
+                    'maxMessage' => "La longueur maximale du fabricant peut être de 50"
+                ]),
+                'label' => 'Fabricant'
+            ])
+            ->add('serialNumber', TextType::class, [
+                'constraints' => new Length([
+                    'min' => 4,
+                    'minMessage'=> "Le nom doit avoir une longueur minimale de 4",
+                    'max' => 255,
+                    'maxMessage' => "La longueur maximale du numéro de série peut être de 255"
+                ]),
+                'label' => 'Numéro de série'
+            ])
             ->add('module', EntityType::class, [
                 'class' => Module::class
             ])
--- src/Form/ModuleType.php
+++ src/Form/ModuleType.php
@@ -5,16 +5,35 @@ namespace App\Form;
 use App\Entity\Module;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ModuleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'constraints' => new Length([
+                    'min' => 3,
+                    'minMessage'=> "Le nom doit avoir une longueur minimale de 3",
+                    'max' => 50,
+                    'maxMessage' => "La longueur maximale du nom peut être de 50"
+                ]),
+                'label' => 'Nom'
+            ])
+            ->add('description', TextareaType::class, [
+                'constraints' => new Length([
+                    'min' => 3,
+                    'minMessage'=> "La description doit avoir une longueur minimale de 3",
+                    'max' => 255,
+                    'maxMessage' => "La longueur maximale de la description peut être de 255"
+                ]),
+                'label' => 'Description'
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
